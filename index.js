8bb50e54526e85feb5a202df9ab52832a64127a5
Vlad Vojvo <vlad.vojvo86@gmail.com>
2021-10-24 22:47:50 +1100

    start of 3a

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express")
+const data = require("./data")
+const bcrypt = require("bcrypt")
+const morgan = require("morgan")
+const db = require("./database")
 
+const app = express()
 const PORT = process.env.PORT || 3000
 
+app.get("/", (req, res)=>{
+    res.send(`${data.users}  "hello world!"`);
+    
+});
 
+
+
+app.listen(PORT, () => {
+    console.log(`eApp is listening on http://localhost:${PORT}`)
+})
